1f42e208355a5dfed638f1b23f9c317a0cca76d8
Ruwaidah Alfakhri <Ruwaidah.riyadh@gmail.com>
2024-02-05 18:49:38 -0600

    adding url socket server

--- api/server.js
+++ api/server.js
@@ -1,10 +1,18 @@
-const express = require("express");
-const middleware = require("./setupMiddleware.js");
+// const express = require("express");
+// const middleware = require("./setupMiddleware.js");
 const users = require("../routes/users.js")
+
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
 
 const app = express();
 
-middleware(app);
+app.use(helmet());
+app.use(express.json());
+app.use(cors());
+
+// middleware(app);
 
 app.use("/api/users", users)
 
