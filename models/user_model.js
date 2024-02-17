c696e51710c75a49c648be1be0d0448850a76cb7
Ruwaidah Alfakhri <Ruwaidah.riyadh@gmail.com>
2024-02-17 16:53:52 -0600

    Adding token and hash password

--- models/user_model.js
+++ models/user_model.js
@@ -1,11 +1,11 @@
 const db = require("../database/dbConfig");
 
-const createUser =  (data) => {
-  return  db("users").insert(data, "id");
-   
+const createUser = (data) => {
+  return db("users").insert(data, "id");
 };
 
 const getUserBy = (data) => {
+  console.log(data)
   return db("users").where(data).first();
 };
 
--- routes/users.js
+++ routes/users.js
@@ -1,27 +1,42 @@
 const router = require("express").Router();
-const User = require("../models/user_model")
+const User = require("../models/user_model");
+const bcrypt = require("bcryptjs");
 const UserState = require("../usersdata");
+const generateToken = require("../generateToken.js");
 
 router.post("/", (req, res) => {
-  console.log(req.body);
-  User.createUser(req.body).then(response => {
-    console.log(response)
-    if (response) {
-
-    } else {
-      res.status()
-    }
-  } ).catch(error => {
-    if (error.code === "23505") {
-      const regex = new RegExp(`${req.body.username}|${req.body.email}|=|Key|[().]`,"g")
-      const msg = error.detail.replace(regex, "")
-      res.status(500).json({message: msg})
-    } else {
-      res.status(500).json({message: "Error create new data"})
-    }
-  })
+  const user = ({ username, password, email } = req.body);
+  user.password = bcrypt.hashSync(user.password, 8);
+  User.createUser(req.body)
+    .then((response) => {
+      console.log("respo", user.password);
+      const token = generateToken(response.id);
+      User.getUserBy(response[0])
+        .then((user) => {
+          res.status(201).json({
+            id: user.id,
+            username: user.username,
+            email: user.email,
+            create_at: user.create_at,
+            token,
+          });
+        })
+        .catch((error) =>
+          res.status(500).json({ message: "Error getting data" })
+        );
+    })
+    .catch((error) => {
+      if (error.code === "23505") {
+        const regex = new RegExp(
+          `${req.body.username}|${req.body.email}|=|Key|[().]`,
+          "g"
+        );
+        const msg = error.detail.replace(regex, "");
+        res.status(500).json({ message: msg });
+      } else {
+        res.status(500).json({ message: "Error create new data" });
+      }
+    });
 });
 
 module.exports = router;
-
-
