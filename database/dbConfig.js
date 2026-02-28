f91e17198dbb8481a4568fb78b21f76274d84b46
Ruwaidah Alfakhri <ruwaidah.riyadh@gmail.com>
2026-02-27 22:06:48 -0600

    fixing bug

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -3,7 +3,7 @@
 import knex from "knex";
 import knexConfig from "../knexfile.js";
 
-const env = process.env.NODE_ENV || "development"; // Render should be production
-console.log("KNEX ENV:", env); // TEMP: to confirm in Render logs
+const env = process.env.NODE_ENV || "development";
+console.log("KNEX ENV:", env);
 
 export default knex(knexConfig[env]);
