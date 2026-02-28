edcee3327a589058d0e57d784d6287a477e56ce1
Ruwaidah Alfakhri <ruwaidah.riyadh@gmail.com>
2026-02-27 20:06:55 -0600

    cleaning

--- knexfile.js
+++ knexfile.js
@@ -44,17 +44,6 @@ export default {
   production: {
     client: "pg",
     connection: process.env.DATABASE_URL,
-    // connection: {
-    //   user: process.env.DB_USER,
-    //   password: process.env.DB_PASSWORD,
-    //   host: process.env.DB_HOST,
-    //   port: process.env.DB_PORT,
-    //   database: process.env.DB_DATABASE_NAME,
-    //   ssl: {
-    //     rejectUnauthorized: true,
-    //     ca: process.env.DB_SSL_CA,
-    //   },
-    // },
     migrations: {
       directory: "./database/migrations",
       tableName: "knex_migrations",
